f016d2b96478c45727953e47f3f706d54c89348c
ThelmaBarrera <134438728+ThelmaBarrera@users.noreply.github.com>
2023-07-01 16:40:09 -0600

    Add files via upload

---
+++ scrimbatrainstation.js
@@ -0,0 +1,21 @@
+// document.getElementById("count").innerText = 5 
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById('count-el')
+let count = 0
+
+function increment() {
+	count += 1
+	countEl.innerText = count
+	console.log(count)
+}
+
+function save(){
+	let countStr = count + " - "
+	saveEl.innerText += countStr
+	countEl.textContent = 0
+	count = 0 
+
+	
+}
+
+
